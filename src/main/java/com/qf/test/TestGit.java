184267b0cb235dd44b5718c432ca712d75a4762c
xiaozhiqiang <1759884863@qq.com>
2020-04-16 22:05:03 +0800

    add a java file

--- src/main/java/com/qf/test/TestGit.java
+++ src/main/java/com/qf/test/TestGit.java
@@ -12,7 +12,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git");
-        System.out.println("git");
-        System.out.println("git");
+
     }
 }
