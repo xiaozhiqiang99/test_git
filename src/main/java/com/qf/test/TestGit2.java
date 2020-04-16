4e957852f2b7429a576c9a3c6caf7bd061d4f581
xiaozhiqiang <1759884863@qq.com>
2020-04-16 22:03:12 +0800

    add a java file

---
+++ src/main/java/com/qf/test/TestGit2.java
@@ -0,0 +1,16 @@
+package com.qf.test;
+
+/**
+ * 功能说明
+ *
+ * @author xiaozhiqiang
+ * @date 2020-04-16 22:01:43
+ */
+public class TestGit2 {
+
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+    }
+}
