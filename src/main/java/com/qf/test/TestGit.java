f783c709d1b5b31057f4069cf4f3d970b2031d41
xiaozhiqiang <1759884863@qq.com>
2020-04-16 21:41:56 +0800

    init file

---
+++ src/main/java/com/qf/test/TestGit.java
@@ -0,0 +1,15 @@
+package com.qf.test;
+
+/**
+ * 功能说明
+ *
+ * @author xiaozhiqiang
+ * @date 2020-04-16 21:35:59
+ */
+public class TestGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
